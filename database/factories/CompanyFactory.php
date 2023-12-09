6f7db00f77d60b31376dde89b8ab6492a15b505d
Amir Ridwan <83332805+mrriddwan@users.noreply.github.com>
2023-12-10 00:11:58 +0800

    feat(seeder.factory): init for company and  employee

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Company::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'        => fake()->name(),
+            'email'       => fake()->email(),
+            'logo'        => fake()->name(),
+            'website_url' => fake()->url(),
+        ];
+    }
+}
