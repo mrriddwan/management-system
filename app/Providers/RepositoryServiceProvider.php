d6245f7589e509aa6ac23cd7f683d3b2eab5443a
Amir Ridwan <83332805+mrriddwan@users.noreply.github.com>
2023-12-09 22:24:16 +0800

    feat(provider.repo): init with employee and company repo

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+                // Employee Repository
+                $this -> app -> bind(\App\Repositories\Employee\EmployeeRepoInterface::class, \App\Repositories\Employee\EmployeeRepository::class);
+
+                // Management Repository
+                $this -> app -> bind(\App\Repositories\Company\CompanyRepoInterface::class, \App\Repositories\Company\CompanyRepository::class);
+        
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
