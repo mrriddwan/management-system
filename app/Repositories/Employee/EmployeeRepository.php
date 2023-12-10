5e7fe668fb21f2bd3d65dce11caae40374236215
Amir Ridwan <83332805+mrriddwan@users.noreply.github.com>
2023-12-10 09:16:07 +0800

    fix(employee.repo): add method

--- app/Repositories/Employee/EmployeeRepository.php
+++ app/Repositories/Employee/EmployeeRepository.php
@@ -12,7 +12,6 @@ class EmployeeRepository implements EmployeeRepoInterface
 
     public function index(): LengthAwarePaginator
     {
-        dd(1);
         return Employee::paginate();
     }
 
@@ -24,14 +23,24 @@ class EmployeeRepository implements EmployeeRepoInterface
     public function create(array $data): Employee
     {
         return Employee::create([
-            'name' => $data['name'],
+            'name'         => $data['name'],
+            'first_name'   => $data['first_name'],
+            'last_name'    => $data['last_name'],
+            'email'        => $data['email'],
+            'phone_number' => $data['phone_number'],
+            'company_id'   => $data['company_id'],
         ]);
     }
 
     public function update(Employee $employee, array $data): Employee
     {
         $employee->update([
-            'name' => $data['name']
+            'name'         => $data['name'],
+            'first_name'   => $data['first_name'],
+            'last_name'    => $data['last_name'],
+            'email'        => $data['email'],
+            'phone_number' => $data['phone_number'],
+            'company_id'   => $data['company_id'],
         ]);
 
         return $employee;
