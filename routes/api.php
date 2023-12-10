9450b90cdef87433dedb4251b690eae786771b88
Amir Ridwan <83332805+mrriddwan@users.noreply.github.com>
2023-12-10 09:14:24 +0800

    fix(api): renamed route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->group(function ()
+// {
+    Route::prefix('company')->group(function ()
+    {
+        \App\Routes\Company\CompanyAPIRoute::V1();
+    });
+
+    Route::prefix('employee')->group(function ()
+    {
+        \App\Routes\Employee\EmployeeAPIRoute::V1();
+    });
+// });
